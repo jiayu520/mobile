6ad8c7277c28cf4e7dcedc1f66bc9f125d7c58c0
jiayu520 <jiayu520000@outlook.com>
2020-04-19 22:08:05 +0800

    Merge branch 'master' of https://github.com/jiayu520/pageobject
    # Conflicts:
    #	.idea/modules.xml
    #	venv/Lib/site-packages/pytest-5.4.1.dist-info/RECORD
    #	venv/Scripts/py.test.exe
    #	venv/Scripts/pytest.exe

--- demon/test_dwpytest.py
+++ demon/test_dwpytest.py
@@ -4,14 +4,26 @@ import pytest
 from appium import webdriver
 class TestDW():
     def setup(self):
-        pass
-
+        desired_caps = {
+            "platformName": "Android",
+            "deviceName": "127.0.0.1:7555",
+            "appPackage": "com.xueqiu.android",
+            "appActivity": ".view.WelcomeActivityAlias",
+            "unicodeKeyBoard" : True,#为了中文输入
+            "resetkeyBoard": True,
+            "noReset": True  # 不重置
+        }
+        self.driver = webdriver.Remote("http://127.0.0.1:4723/wd/hub", desired_caps)
+        self.driver.implicitly_wait(10)
     def teardown(self):
-        pass
+        self.driver.quit()
 
     def test_search(self):
         print("搜索测试用例")
-
-
+        self.driver.find_element_by_id("com.xueqiu.android:id/home_search").click()
+        self.driver.find_element_by_id("com.xueqiu.android:id/search_input_text").send_keys("阿里巴巴 ")
+        self.driver.find_element_by_xpath("//*[@resource-id='com.xueqiu.android:id/name' and @text='阿里巴巴']").click()
+        current_price =  float(self.driver.find_element_by_id("com.xueqiu.android:id/current_price").text)
+        assert current_price  >  200
 if __name__ == '__main__':
     pytest.main()
