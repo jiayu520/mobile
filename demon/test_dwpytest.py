b01c19705a4876a220af0c6176190063d33d27b3
jiayu520 <jiayu520000@outlook.com>
2020-04-19 20:51:02 +0800

    Merge branch 'master' of https://github.com/jiayu520/pageobject
    # Conflicts:
    #	.idea/modules.xml
    #	venv/Lib/site-packages/pytest-5.4.1.dist-info/RECORD
    #	venv/Scripts/py.test.exe
    #	venv/Scripts/pytest.exe

---
+++ demon/test_dwpytest.py
@@ -0,0 +1,17 @@
+import pytest
+
+
+from appium import webdriver
+class TestDW():
+    def setup(self):
+        pass
+
+    def teardown(self):
+        pass
+
+    def test_search(self):
+        print("搜索测试用例")
+
+
+if __name__ == '__main__':
+    pytest.main()
